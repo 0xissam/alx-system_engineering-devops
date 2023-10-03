99082ee157fa2d8584b9965abbcbde1101ea13d0
0xissam <hah305581@gmail.com>
2023-10-03 22:41:07 +0100

    update

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# The regular expression must match School
 
-puts ARGV[0].scan(/School/).join
+regex = /School/
+puts ARGV[0].scan(regex).join
